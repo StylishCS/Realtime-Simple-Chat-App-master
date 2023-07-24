2121904736ecd8e54b417d6b8c7c4de0682489f1
StylishCS <joeshirf@gmail.com>
2023-07-24 16:26:08 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const io = require('socket.io')(3000)
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req,res)=>{
+    res.redirect('http://localhost:3000/index.html');
+})
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+})
+
+
+app.listen(5500);
